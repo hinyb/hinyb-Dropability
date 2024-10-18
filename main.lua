343f5985a604ffc792d66c0508f8c61eb82bde7a
hinyb <40139991+hinyb@users.noreply.github.com>
2024-10-19 00:36:04 +0800

    Rename better variables

--- main.lua
+++ main.lua
@@ -26,7 +26,7 @@ local drop_item_id_list = {}
 local drop_item, drop_item_handler
 local item_tooltip, lang_map, item_cache
 local cached_dup_num
-local is_dropped = false
+local pickup_is_dropped = false
 local function get_player_with_m_id(m_id)
     local players = Instance.find_all(gm.constants.oP)
     for _, p in ipairs(players) do
@@ -75,14 +75,14 @@ gm.pre_script_hook(gm.constants.item_give, function(self, other, result, args)
             cached_dup_num = gm.array_get(other.inventory_item_stack, 95)
             gm.array_set(other.inventory_item_stack, 95, 0)
             table.remove(drop_item_id_list, index)
-            is_dropped = true
+            pickup_is_dropped = true
         end
     end
 end)
 gm.post_script_hook(gm.constants.item_give, function(self, other, result, args)
-    if is_dropped then
+    if pickup_is_dropped then
         cached_dup_num = nil
-        is_dropped = false
+        pickup_is_dropped = false
     end
 end)
 gm.post_script_hook(gm.constants.run_create, function(self, other, result, args)
